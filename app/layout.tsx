14511665d168bd7f7a5337c5acb76b83905a8be8
hyunho <oksknoh@gmail.com>
2023-12-12 21:59:59 +0900

    add landing and link to other sites

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"> */}
+          {/* <div className="mx-auto max-w-3xl"> */}
+            {children}
+          {/* </div> */}
+        {/* </div> */}
+      </body>
     </html>
   )
 }
